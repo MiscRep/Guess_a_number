7b0bf4992dbffd621f6320ce969d7f20822208f7
Ebony B <70521133+MiscRep@users.noreply.github.com>
2022-06-09 21:47:43 -0400

    Version 0.3
    Will now ask player to enter their name

--- Guess the Number.py
+++ Guess the Number.py
@@ -5,3 +5,5 @@ import random
 num_guess = 0
 
 print("Welcome to the Guess the Number game!\n") # Welcomes the user to the game
+
+player_name = input("What should I call you? Please enter your name and press [ENTER]\n")
