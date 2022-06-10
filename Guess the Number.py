a037d46a864906a080057e1741a82f31842b4527
Ebony B <70521133+MiscRep@users.noreply.github.com>
2022-06-09 21:50:02 -0400

    Version 0.4
    x and y have been defined for rand_num

--- Guess the Number.py
+++ Guess the Number.py
@@ -7,3 +7,7 @@ num_guess = 0
 print("Welcome to the Guess the Number game!\n") # Welcomes the user to the game
 
 player_name = input("What should I call you? Please enter your name and press [ENTER]\n")
+
+print(f"Nice to meet you {player_name}! Lets start the game.\n")
+
+rand_num = random.randint(1,20)
